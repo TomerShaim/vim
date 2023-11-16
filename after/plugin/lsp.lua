238e40870269d102ff8728bda47783372367870d
Tomer Shaim <tomer.shaim@cyera.io>
2023-11-16 09:46:44 +0200

    lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,24 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+  'tsserver',
+  'rust_analyzer',
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
